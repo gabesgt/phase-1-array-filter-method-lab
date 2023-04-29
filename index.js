79a6ec3b91f74bf618eb08b6667eb1d7434e3a47
Gabriel Sargent <gabe2sargent@gmail.com>
2023-04-29 07:49:01 -0500

    arLab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(drivers, atturibute ){
+  return drivers.filter(function (driver){
+    return driver.toLowerCase()=== atturibute.toLowerCase()
+
+
+
+  })
+}
+function fuzzyMatch(drivers,atturibute){
+  return drivers.filter(function(driver){
+    return driver.toLowerCase().substring(0,atturibute.length) ===atturibute.toLowerCase()})
+  }
+  function matchName(drivers, argument){
+    return drivers.filter(function(driver){
+      return driver.name === argument
+    })
+  }
+
+
+
+
